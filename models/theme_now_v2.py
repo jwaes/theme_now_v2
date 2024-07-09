4c6bd0e0996232791e6f8bd55c6e0748ca85cc14
jwaes <jaco.waes@gmail.com>
2024-07-09 12:45:01 +0000

    f

--- models/theme_now_v2.py
+++ models/theme_now_v2.py
@@ -12,14 +12,5 @@ class ThemeNowV2(models.AbstractModel):
         return res    
 
     def _theme_now_v2_post_copy(self, mod):
-        # self.enable_view('website.template_header_vertical')
-        # self.enable_view('website.header_navbar_pills_style')
         self.disable_view('website.template_header_default')
         self.enable_view('theme_now_v2.template_header_now')
-
-        # self.enable_view('website.template_footer_centered')
-        # self.enable_view('website.template_footer_slideout')
-        # self.enable_view('website.option_footer_scrolltop')
-
-        # self.enable_asset("website.ripple_effect_scss")
-        # self.enable_asset("website.ripple_effect_js")
